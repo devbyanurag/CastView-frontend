ff3f33428a1c75fba9adc30c15977f18148c93bf
anuragchhetri <karanchheti@gmail.com>
2024-07-18 13:03:24 +0530

    optimize the code for api loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,68 +1,79 @@
-import { useEffect, useState } from 'react'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Home from './pages/Home/Home'
-import Header from './components/Header/Header'
-import Login from './pages/Login/Login'
-import Signup from './pages/Signup/Signup'
-import { ToastContainer } from 'react-toastify';
+import { useEffect, useState, lazy, Suspense } from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { ToastContainer, toast } from 'react-toastify';
+import { useDispatch } from 'react-redux';
+import { login } from './store/slices/authSlice';
+import { apiInstance } from './utils/api';
+import { tokenCastView } from './utils/variables';
 import 'react-toastify/dist/ReactToastify.css';
-import { useDispatch } from 'react-redux'
-import ProtectedRoute from './pages/ProtectedRoute'
-import { apiInstance } from './utils/api'
-import { toast } from 'react-toastify'
-import { login } from './store/slices/authSlice'
-import Loading from './pages/Loading/Loading'
-import { tokenCastView } from './utils/variables'
-import SignupVerify from './pages/SignupVerify/SignupVerify'
+import Header from './components/Header/Header';
+import ProtectedRoute from './pages/ProtectedRoute';
+import Loading from './pages/Loading/Loading';
+import WaitServer from './pages/WaitServer/WaitServer';
 
+const Home = lazy(() => import('./pages/Home/Home'));
+const Login = lazy(() => import('./pages/Login/Login'));
+const Signup = lazy(() => import('./pages/Signup/Signup'));
+const SignupVerify = lazy(() => import('./pages/SignupVerify/SignupVerify'));
 
 const App = () => {
-  const dispatch = useDispatch()
-  const [loading, setLoading] = useState(true)
-  // console.log(import.meta.env.MODE)
+  const dispatch = useDispatch();
+  const [loading, setLoading] = useState(true);
+  const [loadingServer, setLoadingServer] = useState(false);
 
   useEffect(() => {
     const verifyLogin = async () => {
       let token = localStorage.getItem(tokenCastView);
       if (token) {
+        const timeout = setTimeout(() => {
+          setLoadingServer(true);
+        }, 10000); // 10 seconds
+
         try {
           const response = await apiInstance.get(`user/verifyLogin/${token}`);
-          console.log('User logged in:', response.data);
-          if (response.status == 200) {
+          clearTimeout(timeout);
+
+          if (response.status === 200) {
             dispatch(login(response.data.user));
-          }
-          else {
-            localStorage.removeItem(tokenCastView)
+          } else {
+            localStorage.removeItem(tokenCastView);
           }
         } catch (error) {
+          clearTimeout(timeout);
           toast.error('Login failed. Please try again.');
         }
       }
-      setLoading(false)
+      setLoading(false);
     };
 
     verifyLogin();
-  }, [])
+  }, [dispatch]);
 
-
+  if (loading) {
+    return loadingServer ? <WaitServer /> : <Loading />;
+  }
 
   return (
-    loading ? <Loading /> :
-
-      <BrowserRouter>
-        <Header />
+    <BrowserRouter>
+      <Header />
+      <Suspense fallback={<Loading />}>
         <Routes>
-          <Route path='/' element={<ProtectedRoute>
-            <Home />
-          </ProtectedRoute>} />
+          <Route
+            path='/'
+            element={
+              <ProtectedRoute>
+                <Home />
+              </ProtectedRoute>
+            }
+          />
           <Route path='/login' element={<Login />} />
           <Route path='/signup' element={<Signup />} />
           <Route path='/signupVerify/:token' element={<SignupVerify />} />
         </Routes>
-        <ToastContainer />
-      </BrowserRouter>
+      </Suspense>
+      <ToastContainer />
+    </BrowserRouter>
+  );
+};
 
-  )
-}
-
-export default App
+export default App;
---
+++ src/pages/WaitServer/WaitServer.tsx
@@ -0,0 +1,14 @@
+
+const WaitServer = () => {
+    return (
+        <div className='h-screen w-full bg-black flex justify-center items-center '>
+            <div className='bg-white w-4/5 md:w-1/2 lg:w-96 p-5 rounded-md'>
+                <p>Please wait we are using a free server which sleeps on inactivity. The server will start in 1 min please wait.</p>
+                <p>Thank you for you patience.</p>
+
+            </div>
+        </div>
+    )
+}
+
+export default WaitServer
